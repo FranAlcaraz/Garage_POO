ae84c2f648d2055652c7e76974e3cb8b307ceca2
Francisco Alcaraz <fran.alcaraz@live.com>
2018-07-28 18:16:45 -0300

    Revert "Revert "ahi tenemos todo""
    This reverts commit 7af3e5d9df9694433db69e55bbd7edbe23d180b0.

--- src/garage/Cliente.java
+++ src/garage/Cliente.java
@@ -4,13 +4,18 @@ public class Cliente {
 	
 	private String nCliente;
 	private String tipoV;
-	private Auto auto;
+	private Auto[] auto;
 	private Moto[] moto;
 	private int numeroDeMoto;
+	private int numeroDeAuto;
 	
 	public Cliente (String nombre, String tipoVehiculo){
 		nCliente =nombre;
 		tipoV = tipoVehiculo;
+		moto = new Moto[10];
+		numeroDeMoto = 0;
+		auto = new Auto[10];
+		numeroDeAuto = 0;
 	}
 	
 	public void Moto (String tipoV, String m, int cilindrada, int kilometraje, int cRuedas){
@@ -18,6 +23,10 @@ public class Cliente {
 		moto[i] = new Moto (tipoV, m, cilindrada, kilometraje, cRuedas);
 	}
 	
+	public void Auto (String tipoV, String m, int cPuertas, int kilometraje, int cRuedas){
+		int i = numeroDeAuto++;
+		auto[i] = new Auto (tipoV, m, cPuertas, kilometraje, cRuedas);
+	}
 	
 	public String getnCliente() {
 		return nCliente;
