1df181a5dd5ae821caab5d07b97196c093541acd
Francisco Alcaraz <fran.alcaraz@live.com>
2018-07-28 17:56:31 -0300

    commentario por errores

--- src/garage/GarageTest.java
+++ src/garage/GarageTest.java
@@ -1,9 +1,21 @@
 package garage;
+import garage.Garage;;
 
 public class GarageTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+//		public void sumarVehiculo(){
+//			vehiculos = ingAuto + ingMoto;
+//		}
+//		public void alertaMax(){
+//			if (vehiculos>=10){
+//			System.out.println("No caben mas vehiculos...lo sentimos!");
+//			}
+//			
+//		}
 
 	}
 
